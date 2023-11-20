6e99e1b54c116802d4c1f6371f88a92dd32c3306
seominki <minki@paymint.co.kr>
2023-11-20 17:42:23 +0900

    크리스마스 메시지 게시판

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # merry-christmas
+
+## 크리스마스 메시지 게시판
+
+### supabase 데이터베이스 사용
+
+[https://supabase.com/]
---
+++ js/supabase.js
@@ -0,0 +1,53 @@
+import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+ // Create a single supabase client for interacting with your database
+ const supabase = createClient('https://yicdppmuskaspvvcwhqs.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlpY2RwcG11c2thc3B2dmN3aHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA0NDczNDQsImV4cCI6MjAxNjAyMzM0NH0.0xBZia1hA38Tlm9LJB3XzFTxvdXJoBkNeMbe0cy30es');
+
+document.getElementById('register').addEventListener('click', getInsertInfo);
+
+async function getMessageList(){
+    const {data, error} =  await supabase
+    .from('message_list')
+    .select("*");
+
+    const $list = document.getElementById("messageList");
+    
+    $list.innerHTML = data.map((info, index) => {
+        return `<li>
+                    <span>${info.charactor}</span>
+                    <span>${info.name}</span>
+                    <span>${info.message}</span>
+                </li>`
+    }).join('');    
+}
+
+ function getInsertInfo(){
+    const $charactor = document.getElementById('charactor').value;
+    const $name = document.getElementById('name').value;
+    const $message = document.getElementById('message').value;
+
+    if(!$charactor || !$name || !$message){
+        return alert("정보를 입력해주세요!");
+    }
+
+    insertMessage($charactor, $name, $message);
+}
+
+async function insertMessage($charactor, $name, $message){
+    const {data,error} = await supabase
+    .from('message_list')
+    .upsert({ 
+        charactor: $charactor, 
+        name: $name,
+        message : $message,
+        // registerDate : new Date().getTime()
+    })
+    .select()
+
+    if(error){
+        return alert("에러");
+    }
+
+    getMessageList();
+}
+
+getMessageList();
